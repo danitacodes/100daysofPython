ee10388331a5ec53cb0b2182cc510796d7acb8e6
Danita Codes <danitacodes@gmail.com>
2023-01-29 08:48:49 -0800

    coded python adventure game

--- Day3 - Treasure Island/Adventure/main.py
+++ Day3 - Treasure Island/Adventure/main.py
@@ -13,4 +13,19 @@ print('''
 print("Welcome to Fantasy Island.")
 print("Your task is to find love.")
 
-first_part = input("You have arrived at Fantasy Island. You have a choice to go to the left or right side of the island. What say you? Type 'left' or 'right'")
+first_part = input("You have arrived at Fantasy Island. You have a choice to go to the left or right side of the island. What say you? Type 'left' or 'right' \n").lower()
+
+
+#Conditional
+if first_part == 'left':
+  second_part = input("You arrive at this beautiful stream. You see another island in the distance. Do you swim to it or wait for a boat? Type 'swim' or 'boat' \n").lower()
+  if second_part == 'swim':
+    third_part = input("You've made it to the tiny island. There are three doors to choose from. Red, Blue, and Green. Which one you choosing? Type 'red', 'blue', or 'green'. \n").lower()
+    if third_part == 'blue':
+      print("You've found the love of your life.")
+    else:
+      print("You chose the wrong door. Game Over!")
+  else:
+    print("You waited too long for a boat and died from sun exposure. Game Over!")
+else:
+  print("You've chosen the path of lonely hearts. Game Over!")
